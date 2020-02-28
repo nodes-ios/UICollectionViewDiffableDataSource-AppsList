cb171094585ad4593315a7471a1d8c9a5430fe58
Bob De Kort <bogo@nodes.dk>
2020-02-28 09:48:12 +0100

    Initial Commit

---
+++ AppsList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppsList
+//
+//  Created by Bob De Kort on 28/02/2020.
+//  Copyright © 2020 Nodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
